082d651576466c109a49a00ca5e9c851590ceab6
Valeria Benetti <vbenetti46@gmail.com>
2017-05-18 19:40:38 -0400

    Added paths for new user
    Still need to make the pages show for the log in and sign up then I should be able to make it to where only the people adding the test who work for the company are able to log in and either modify the information or delete the test it self

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  attr_accessor :password
+  validates :email, presence: true, uniqueness: true
+  validates :password, confirmation: true # password_confirmation attr
+  validates_length_of :password, in: 6..20, on: :create
+  has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
 
   root 'pages#landing'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 end
