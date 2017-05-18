e668a3f9159a956a1816ec54ae5499ff1e8fee73
Valeria Benetti <vbenetti46@gmail.com>
2017-05-18 19:00:30 -0400

    Added users
    Will need to ensure that only the admin can log in and create new tests to add to the catalog

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def landing
+    @tests = Test.all
+
+    def show
+      @test = Test.find(params[:id])
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518191835) do
+ActiveRecord::Schema.define(version: 20170518225745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 20170518191835) do
     t.integer "process_identifier"
     t.string "process_name"
     t.text "process_intructions"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
