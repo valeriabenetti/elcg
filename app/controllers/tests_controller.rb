4ca902a5e6fdcfbd34cf3cb6416c91abe673e511
Valeria Benetti <vbenetti46@gmail.com>
2017-05-22 13:33:45 -0400

    Linking to the show individual test is now active

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -8,4 +8,11 @@ class TestsController < ApplicationController
   def show
     @test = Test.find(params[:id])
   end
+
+  private
+
+  # Only allow a trusted parameter "white list" through.
+  def test_params
+    params.require(:test).permit(:id, :procedure_identifier, :procedure_name, :procedure_master_number, :synonyms, :test_code, :category_code, :category_name, :eppicare_inactive_flag, :orderable, :collection_identifier, :collection_name, :collection_instructions, :process_identifier, :process_name, :process_intructions)
+  end
 end
