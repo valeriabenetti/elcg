e952994272fcafc054563d831d504c791dff6e96
Valeria Benetti <vbenetti46@gmail.com>
2017-06-07 22:00:48 -0400

    Testing out the admin abilities

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -18,44 +18,6 @@ class TestsController < ApplicationController
     @test = Test.find(params[:id])
   end
 
-  # GET /tests/new
-  def new
-    @test = Test.new
-  end
-
-  # GET /tests/1/edit
-  def edit
-    @test = Test.find(params[:id])
-  end
-
-  # POST /tests
-  def create
-    @test = Test.new(test_params)
-
-    if @test.save
-      redirect_to @test, notice: 'Test was successfully created.'
-    else
-      render :new
-    end
-  end
-
-  # PATCH/PUT /tests/1
-  def update
-    @test = Test.find(params[:id])
-    if @test.update(test_params)
-      redirect_to @test, notice: 'Test was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /tests/1
-  def destroy
-    @test = Test.find(params[:id])
-    @test.destroy
-    redirect_to tests_url, notice: 'Test was successfully destroyed.'
-  end
-
   def favorite
     test = Test.find(params[:id])
 
