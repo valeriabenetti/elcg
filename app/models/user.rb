f0d5fb38c234c222491b341442e81eee02844506
Valeria Benetti <vbenetti46@gmail.com>
2017-06-13 20:03:14 -0400

    Admin is still not up an running

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_one :profile
   validates :email, presence: true, uniqueness: true
   validates :password, confirmation: true # password_confirmation attr
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :admins
   # For Users
   resources :users
