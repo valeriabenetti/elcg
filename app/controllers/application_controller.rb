3aadd84beddf07238247104eb63d5e96df8f4393
Valeria Benetti <vbenetti46@gmail.com>
2017-06-13 21:28:26 -0400

    Adding new admin paths

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :authorize
+
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
@@ -15,5 +17,13 @@ class ApplicationController < ActionController::Base
 
   def authorize!
     redirect_to login_path unless logged_in?
+  end
+
+  protected
+
+  def authorize
+    unless User.find_by(id: session[:user_id])
+      redirect_to login_path, notice: 'Please log in or sign up'
+    end
   end
 end
