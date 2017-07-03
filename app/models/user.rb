cfe7f78526d89b2bbb9a4766f7139d5130bf3547
Valeria Benetti <vbenetti46@gmail.com>
2017-07-02 19:00:57 -0700

    Fixed the log in issue
    Need to re-add the image processing for users to be able to add the tube color

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
   has_one :profile
   validates :email, presence: true, uniqueness: true
   validates :password, confirmation: true # password_confirmation attr
