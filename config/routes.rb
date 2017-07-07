77ee05ebff52c1d50aca900c7ba2258d0616de1f
Valeria Benetti <vbenetti46@gmail.com>
2017-07-07 11:52:29 -0400

    Adding forgot password and rest token to the application

--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,8 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+
+  post 'password/forgot' => 'password#forgot'
+  post 'password/reset' => 'password#reset'
+  put 'password/update' => 'password#update'
 end
