f1d22d4692bd3d99385acaac439155cb7195df67
Valeria Benetti <vbenetti46@gmail.com>
2017-07-20 18:17:20 -0400

    Pictures are now working

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -76,6 +76,6 @@ class TestsController < ApplicationController
 
   # Only allow a trusted parameter "white list" through.
   def test_params
-    params.require(:test).permit(:procedure_identifier, :procedure_name, :procedure_master_number, :synonyms, :test_code, :category_code, :category_name, :eppicare_inactive_flag, :orderable, :collection_identifier, :collection_name, :collection_instructions, :process_identifier, :process_name, :process_intructions)
+    params.require(:test).permit(:procedure_identifier, :procedure_name, :procedure_master_number, :synonyms, :test_code, :category_code, :category_name, :eppicare_inactive_flag, :orderable, :collection_identifier, :collection_name, :collection_instructions, :process_identifier, :process_name, :process_intructions, :image)
   end
 end
