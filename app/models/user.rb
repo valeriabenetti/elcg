e73bbabdcd1885f2032f2d1ba4a8191b20e152d4
Valeria Benetti <vbenetti46@gmail.com>
2017-07-23 17:06:08 -0400

    Secured the Password and Email of Users even more

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,9 @@
 class User < ApplicationRecord
-  before_create { generate_token(:auth_token)}
-
-  def send_password_reset
-    generate_token(:password_reset_token)
-    self.password_reset_sent_at = Time.zone.now
-    save!
-    UserMailer.password_reset(self).deliver
-  end
-
-  def generate_token(column)
-    begin
-      self[column] = SecureRandom.urlsafe_base64
-    end while User.exists?(column => self[column])
-  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_one :profile
-  validates :email, presence: true, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false}
   validates :password, confirmation: true # password_confirmation attr
   validates_length_of :password, in: 6..20, on: :create
 
